0be2a88d65184a23149b3e0b55f6afcaac823a48
Omar Nabi <onabi20@gmail.com>
2020-06-12 03:46:38 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+    get '/' do
+    "Hello, World!"
+  end
+    
+  get '/name' do 
+    "My name is Omar Nabi"
+  end
+  
+  get '/hometown' do 
+    "My hometown is Herat"
+  end
+  
+  get '/favorite-song' do 
+    "My favorite song is Who You Foolin"
+  end
+  
 end
